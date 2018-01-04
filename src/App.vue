2a47a2c36cb7a6044f69ee7d0e6bd84376cb07c2
洪文乐 <noclip@foxmail.com>
2018-01-04 15:06:07 +0800

    框

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,65 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-container>
+      <el-header>头部</el-header>
+      <el-container :style="height">
+        <el-aside width="200px">
+          <el-menu>
+            <el-submenu
+              v-for = "(item, i) in menu"
+              :key = "i"
+              :index = "i+''">
+              <template slot="title">{{ item.title }}</template>
+              <el-menu-item-group
+                v-for="(sub, j) in item.sub"
+                :key="j">
+                <el-menu-item
+                  :index="i+'-'+j"
+                  @click="clickMenu(sub.route)">
+                  {{ sub.title }}
+                </el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+      <el-footer>底部</el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+      height: {
+        height: (document.documentElement.clientHeight - 120) + 'px'
+      },
+      menu: [
+        {
+          title: '长租管理',
+          sub: [{
+            title: '长租套餐管理',
+            route: 'page1'
+          }, {
+            title: '长租套餐价格管理',
+            route: 'page2'
+          }]
+        },
+        {
+          title: '以租代售',
+          sub: [{
+            title: '以租代售套餐',
+            route: 'page3'
+          }]
+        }
+      ]
+    }
+  }
 }
 </script>
 
