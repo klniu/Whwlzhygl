e57f183b72a5f118cbfe5365c4acb791a81db59b
Noclip <noclip@foxmail.com>
2018-01-11 20:47:29 +0800

    客户管理

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Notice from '@/pages/notice'
 import NoticeEdit from '@/pages/notice-edit'
 import Accident from '@/pages/accident'
 import AccidentEdit from '@/pages/accident-edit'
+import Customer from '@/pages/customer'
+import CustomerEdit from '@/pages/customer-edit'
 
 Vue.use(Router)
 
@@ -29,6 +31,14 @@ export default new Router({
       path: '/accident-edit',
       name: 'AccidentEdit',
       component: AccidentEdit
+    }, {
+      path: '/customer',
+      name: 'Customer',
+      component: Customer
+    }, {
+      path: '/customer-edit',
+      name: 'CustomerEdit',
+      component: CustomerEdit
     }
   ]
 })
