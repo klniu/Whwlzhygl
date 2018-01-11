0695bc70f64585677e4909be38ec088443aaf00b
Noclip <noclip@foxmail.com>
2018-01-11 21:37:05 +0800

    客户管理-列表、编辑

--- src/pages/customer-edit.vue
+++ src/pages/customer-edit.vue
@@ -1,10 +1,105 @@
 <template>
-  <div></div>
+  <div>
+    <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="100px" size="medium">
+      <el-form-item label="客户地址 " prop="customerAddress">
+        <el-input v-model="formData.customerAddress"></el-input>
+      </el-form-item>
+      <el-form-item label="客户名称" prop="customerName">
+        <el-input v-model="formData.customerName"></el-input>
+      </el-form-item>
+      <el-form-item label="联系人姓名" prop="linkmanName">
+        <el-input v-model="formData.linkmanName"></el-input>
+      </el-form-item>
+      <el-form-item label="联系人号码" prop="linkmanMobile">
+        <el-input v-model="formData.linkmanMobile"></el-input>
+      </el-form-item>
+      <el-form-item label="收货地址" prop="receiveAddress">
+        <el-input v-model="formData.receiveAddress"></el-input>
+      </el-form-item>
+      <el-form-item label="发货地址" prop="sendAddress">
+        <el-input v-model="formData.sendAddress"></el-input>
+      </el-form-item>
+      <el-form-item label="证件" prop="pic">
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :file-list="fileList2"
+          list-type="picture-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')" :loading="posting">保存</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      id: parseInt(this.$route.query.id),
+      formData: {
+        companyId: 1,
+        customerAddress: '',
+        customerName: '',
+        linkmanMobile: '',
+        linkmanName: '',
+        receiveAddress: '',
+        sendAddress: '',
+        pic: []
+      },
+      rules: {},
+      posting: false,
+      fileList2: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+    }
+  },
+  mounted() {
+    this.id && this.getDetail()
+  },
+  methods: {
+    async getDetail() {
+      let {data} = await this.$http({
+        url: 'customer/getCustomer',
+        params: {
+          customerId: this.id
+        }
+      })
+      if (data.code == 0) {
+        this.formData = data.data
+        this.formData.id = this.id
+      }
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.postForm()
+        } else {
+          this.$message.error('错了哦，这是一条错误消息')
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    async postForm() {
+      this.posting = true
+      let {data} = await this.$http({
+        method: 'post',
+        url: 'customer/' + (this.id ? 'updateCustomer' : 'addCustomer'),
+        data: this.formData
+      })
+      if (data.code == 0) {
+        this.$message({
+          message: '保存成功！',
+          type: 'success'
+        })
+      } else {
+        this.$message.error(data.msg)
+      }
+      this.posting = false
+    }
   }
 }
 </script>
--- src/pages/customer.vue
+++ src/pages/customer.vue
@@ -1,10 +1,121 @@
 <template>
-  <div>Customer</div>
+  <div>
+    <div class="tools">
+      <el-button size="small" icon="el-icon-circle-plus-outline" type="primary" @click="editClick(0)">添加</el-button>
+      <el-button size="small" icon="el-icon-delete" type="danger" @click="delectClick(multipleSelection)">删除</el-button>
+      <el-input v-model="keyword" placeholder="商户名" size="small" class="search-input"></el-input>
+      <el-button size="small" icon="el-icon-search" type="primary" @click="search">搜索</el-button>
+    </div>
+    <el-table
+      ref="multipleTable"
+      :data="tableData.list"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column
+        type="selection">
+      </el-table-column>
+      <el-table-column
+        prop="customerName"
+        label="客户名称">
+      </el-table-column>
+      <el-table-column
+        prop="sendAddress"
+        label="收货地址">
+      </el-table-column>
+      <el-table-column
+        prop="receiveAddress"
+        label="发货地址">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button @click="editClick(scope.row.customerId)" type="text" size="small">编辑</el-button>
+          <el-button @click="delectClick([scope.row.customerId])" type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="height: 20px"></div>
+    <el-pagination
+      @size-change="handleSizeChange"
+      :current-page="page"
+      :page-sizes="[10, 25, 50, 100]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="tableData.total">
+    </el-pagination>
+  </div>
 </template>
+
 <script>
 export default {
   data() {
-    return {}
+    return {
+      page: 1,
+      pageSize: 10,
+      tableData: [],
+      multipleSelection: [],
+      keyword: ''
+    }
+  },
+  mounted() {
+    this.getList()
+  },
+  methods: {
+    search() {},
+    async getList() {
+      const loading = this.$loading()
+      let {data} = await this.$http({
+        url: 'customer/getCustomerList',
+        params: {
+          companyId: 1,
+          currentPage: this.page,
+          size: this.pageSize
+        }
+      })
+      if (data.code == 0) {
+        this.tableData = data.data
+      }
+      loading.close()
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    },
+    editClick(val) {
+      console.log(val)
+      this.$router.push({
+        name: 'CustomerEdit',
+        query: {
+          id: val
+        }
+      })
+    },
+    delectClick(val) {
+      this.$message({
+        message: '删除成功！',
+        type: 'success'
+      })
+    }
   }
 }
 </script>
+<style>
+.search-input{
+  width: 180px;
+  margin-left: 50px;
+}
+</style>
