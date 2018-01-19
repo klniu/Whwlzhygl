c7d1c2871c656efd6417e3f4b9991cb47da42e61
Noclip <noclip@foxmail.com>
2018-01-19 14:47:24 +0800

    通知编辑页面

--- src/pages/notice-edit.vue
+++ src/pages/notice-edit.vue
@@ -1,33 +1,19 @@
 <template>
   <div>
     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="100px">
-      <el-form-item label="通知标题" prop="name" required>
-        <el-input v-model="formData.name"></el-input>
+      <el-form-item label="通知标题" prop="noticeTitle" required>
+        <el-input v-model="formData.noticeTitle"></el-input>
       </el-form-item>
-      <el-form-item label="通知内容" prop="desc">
-        <el-input type="textarea" v-model="formData.desc"></el-input>
+      <el-form-item label="通知内容" prop="noticeContent">
+        <el-input type="textarea" v-model="formData.noticeContent"></el-input>
       </el-form-item>
-      <el-form-item label="活动区域" prop="type">
-        <el-select v-model="formData.type" placeholder="请选择类型">
-          <el-option label="类型1" value="0"></el-option>
-          <el-option label="类型2" value="1"></el-option>
+      <el-form-item label="通知类型" prop="noticeTypeId">
+        <el-select v-model="formData.noticeTypeId" placeholder="请选择类型">
+          <el-option v-for="item in typeList" :key="item.id" :label="item.typeName" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="通知附件" prop="file">
-        <el-upload
-          class="upload-demo"
-          action="https://jsonplaceholder.typicode.com/posts/"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :before-remove="beforeRemove"
-          multiple
-          :limit="3"
-          :on-exceed="handleExceed"
-          :file-list="fileList"
-          list-type="picture">
-          <el-button size="small" type="primary">点击上传</el-button>
-          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
+        <el-input v-model="formData.file"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
@@ -37,46 +23,50 @@
   </div>
 </template>
 <script>
+// TODO: 附件上传
+import saveMixin from '@/mixins/saveform'
 export default {
+  mixins: [saveMixin],
   data() {
     return {
+      id: parseInt(this.$route.query.id),
       formData: {
-        name: '',
-        type: '',
-        desc: ''
+        companyId: 1,
+        noticeContent: '',
+        noticeTitle: '',
+        noticeTypeId: 1
       },
       rules: {},
-      fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+      apiName: 'notice/',
+      addApi: 'addNotice',
+      updateApi: 'updateNotice',
+      typeList: []
     }
   },
+  mounted() {
+    this.id && this.getDetail()
+    this.getTypeList()
+  },
   methods: {
-    handleRemove(file, fileList) {
-      console.log(file, fileList);
+    async getTypeList() {
+      let {data} = await this.$http({
+        url: 'noticeType/getNoticeTypeList'
+      })
+      if (data.code == 0) {
+        this.typeList = data.data.list
+      }
     },
-    handlePreview(file) {
-      console.log(file);
-    },
-    handleExceed(files, fileList) {
-      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-    },
-    beforeRemove(file, fileList) {
-      return this.$confirm(`确定移除 ${ file.name }？`);
-    },
-    submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          this.$message({
-            message: '保存成功！',
-            type: 'success'
-          })
-        } else {
-          this.$message.error('错了哦，这是一条错误消息')
-          return false;
+    async getDetail() {
+      let {data} = await this.$http({
+        url: 'notice/getNotice',
+        params: {
+          noticeId: this.id
         }
-      });
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
+      })
+      if (data.code == 0) {
+        this.formData = data.data
+        this.formData.id = this.id
+      }
     }
   }
 }
