5449f61c40bdff9a71922c475accf260855508f7
Noclip <noclip@foxmail.com>
2018-01-26 22:01:39 +0800

    人员编辑页面

--- src/pages/person-edit.vue
+++ src/pages/person-edit.vue
@@ -1,46 +1,89 @@
 <template>
   <div>
-    <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="120px">
-      <el-form-item label="培训会议主题" prop="meetingTitle" required>
-        <el-input v-model="formData.meetingTitle"></el-input>
+    <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="180px">
+      <el-form-item label="人员名称" prop="personName">
+        <el-input v-model="formData.personName"></el-input>
       </el-form-item>
-      <el-form-item label="参加人员" prop="joinPerson">
-        <el-input v-model="formData.joinPerson"></el-input>
-      </el-form-item>
-      <el-form-item label="参加地址" prop="joinAddress">
-        <el-input v-model="formData.joinAddress"></el-input>
-      </el-form-item>
-      <el-form-item label="主持人" prop="compere">
-        <el-input v-model="formData.compere"></el-input>
-      </el-form-item>
-      <el-form-item label="记录人" prop="recordPerson">
-        <el-input v-model="formData.recordPerson"></el-input>
-      </el-form-item>
-      <el-form-item label="会议内容" prop="meetingContent">
-        <el-input type="textarea" v-model="formData.meetingContent"></el-input>
-      </el-form-item>
-      <el-form-item label="会议时间" prop="meetingDate">
-        <el-date-picker v-model="formData.meetingDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
-      </el-form-item>
-      <el-form-item label="签到" prop="signInIds">
-        <el-select v-model="formData.signInIds" multiple placeholder="请选择">
-          <el-option
-            v-for="item in personList"
-            :key="item.personId"
-            :label="item.personName"
-            :value="item.personId">
-          </el-option>
+      <el-form-item label="人员类型" prop="personTypeId">
+        <el-select v-model="formData.personTypeId" placeholder="请选择">
+          <el-option v-for="item in personTypeList" :key="item.id" :label="item.typeName" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="附件" prop="pic">
+      <el-form-item label="性别" prop="gender">
+        <el-radio-group v-model="formData.gender">
+          <el-radio :label="0">男</el-radio>
+          <el-radio :label="1">女</el-radio>
+          <el-radio :label="2">其他</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="年龄" prop="age">
+        <el-input v-model="formData.age"></el-input>
+      </el-form-item>
+      <el-form-item label="出生日期" prop="birthday">
+        <el-date-picker v-model="formData.birthday" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="民族" prop="nation">
+        <el-input v-model="formData.nation"></el-input>
+      </el-form-item>
+      <el-form-item label="身份证地址" prop="address">
+        <el-input v-model="formData.address"></el-input>
+      </el-form-item>
+      <el-form-item label="身份证号" prop="idCardNum">
+        <el-input v-model="formData.idCardNum"></el-input>
+      </el-form-item>
+      <el-form-item label="联系号码" prop="mobile">
+        <el-input v-model="formData.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="身份证图片" prop="idCardPath">
         <el-upload
+          class="small"
           :action="$baseURL + 'accessory/addAccessory'"
-          :file-list="fileList"
-          :on-success="handleUpload"
-          :on-remove="handleRemove">
-          <el-button size="small" type="primary">点击上传</el-button>
+          :file-list="picsList1"
+          :on-success="handleUpload1"
+          :on-remove="handleRemove1"
+          :on-preview="handlePictureCardPreview"
+          list-type="picture-card">
+          <i class="el-icon-plus"></i>
         </el-upload>
       </el-form-item>
+      <el-form-item label="发证机关" prop="issuingAuthority">
+        <el-input v-model="formData.issuingAuthority"></el-input>
+      </el-form-item>
+      <el-form-item label="身份证有效期开始日期" prop="idCardValidityStartDate">
+        <el-date-picker v-model="formData.idCardValidityStartDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="身份证有效期截止日期" prop="idCardValidityEndDate">
+        <el-date-picker v-model="formData.idCardValidityEndDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="从业资格证类型id" prop="qualificationCertificateTypeId">
+        <el-select v-model="formData.qualificationCertificateTypeId" placeholder="请选择">
+          <el-option v-for="item in certTypeList" :key="item.id" :label="item.typeName" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="初次领证日期" prop="qualificationFirstDate">
+        <el-date-picker v-model="formData.qualificationFirstDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="从业资格证有效期截止日期" prop="qualificationValidityEndDate">
+        <el-date-picker v-model="formData.qualificationValidityEndDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="从业资格证有效期开始日期" prop="qualificationValidityStartDate">
+        <el-date-picker v-model="formData.qualificationValidityStartDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="从业资格证图片" prop="qualificationLicensePath">
+        <el-upload
+          class="small"
+          :action="$baseURL + 'accessory/addAccessory'"
+          :file-list="picsList2"
+          :on-success="handleUpload2"
+          :on-remove="handleRemove2"
+          :on-preview="handlePictureCardPreview"
+          list-type="picture-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+      <el-dialog :visible.sync="dialogVisible" append-to-body>
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
         <el-button @click="resetForm('ruleForm')">重置</el-button>
@@ -49,7 +92,6 @@
   </div>
 </template>
 <script>
-// TODO: 人员管理编辑页
 import saveMixin from '@/mixins/saveform'
 import uploadMixin from '@/mixins/upload'
 export default {
@@ -58,57 +100,85 @@ export default {
     return {
       id: parseInt(this.$route.query.id),
       formData: {
-        companyId: 1,
-        meetingTitle: '',
-        joinPerson: '',
-        joinAddress: '',
-        compere: '',
-        recordPerson: '',
-        meetingContent: '',
-        meetingDate: '',
-        accessoryNames: ''
+        personName: '',
+        personTypeId: '',
+        gender: '',
+        age: '',
+        birthday: '',
+        nation: '',
+        address: '',
+        idCardNum: '',
+        mobile: '',
+        idCardPath: '',
+        issuingAuthority: '',
+        idCardValidityStartDate: '',
+        idCardValidityEndDate: '',
+        qualificationCertificateTypeId: '',
+        qualificationFirstDate: '',
+        qualificationValidityEndDate: '',
+        qualificationValidityStartDate: '',
+        qualificationLicensePath: ''
       },
       rules: {},
-      apiName: 'meeting/',
-      addApi: 'addMeeting',
-      updateApi: 'updateMeeting',
-      fileList: [],
-      personList: []
+      apiName: 'person/',
+      addApi: 'addPerson',
+      updateApi: 'updatePerson',
+      picsList1: [],
+      picsList2: [],
+      personTypeList: [],
+      certTypeList: []
     }
   },
   mounted() {
-    this.getPersonList()
+    this.getPersonTypeList()
+    this.getCertTypeList()
     this.id && this.getDetail()
   },
   methods: {
-    async getPersonList() {
-      let {data} = await this.$http('person/getPersonListAll')
+    async getPersonTypeList() {
+      let {data} = await this.$http('personType/getPersonTypeList')
       if (data.code == 0) {
-        this.personList = data.data
+        this.personTypeList = data.data.list
       }
     },
-    handleRemove(file, list) {
-      this.fileList = list
+    async getCertTypeList() {
+      let {data} = await this.$http('qualificationCertificateType/getQualificationCertificateTypeList')
+      if (data.code == 0) {
+        this.certTypeList = data.data.list
+      }
     },
-    handleUpload(res) {
+    handleRemove1(file, list) {
+      this.picsList1 = list
+    },
+    handleUpload1(res) {
       if (res.code == 0) {
-        this.fileList.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
+        this.picsList1.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
+      }
+    },
+    handleRemove2(file, list) {
+      this.picsList2 = list
+    },
+    handleUpload2(res) {
+      if (res.code == 0) {
+        this.picsList2.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
       }
     },
     beforePost() {
-      this.formData.accessoryNames = this.joinPicIntoString(this.fileList)
+      this.formData.idCardPath = this.joinPicIntoString(this.picsList1)
+      this.formData.qualificationLicensePath = this.joinPicIntoString(this.picsList2)
     },
     async getDetail() {
       let {data} = await this.$http({
-        url: 'meeting/getMeeting',
+        url: 'person/getPerson',
         params: {
-          meetingId: this.id
+          personId: this.id
         }
       })
       if (data.code == 0) {
         this.formData = data.data
         this.formData.id = this.id
-        this.fileList = this.pushPicInitList(this.formData.accessoryNames)
+        this.picsList1 = this.pushPicInitList(this.formData.idCardPath)
+        this.picsList2 = this.pushPicInitList(this.formData.qualificationLicensePath)
       }
     }
   }
