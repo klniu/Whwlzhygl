52104aeb06078bae386a3df11ecef001a460b3d1
Noclip <noclip@foxmail.com>
2018-01-27 22:17:58 +0800

    车辆门检记录编辑

--- src/pages/doorcheck-edit.vue
+++ src/pages/doorcheck-edit.vue
@@ -2,7 +2,15 @@
   <div>
     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="150px" size="medium">
       <el-form-item label="汽车id" prop="carId">
-        <el-input v-model="formData.carId"></el-input>
+        <el-autocomplete
+          v-model="strPlateNum"
+          :fetch-suggestions="getCarList"
+          placeholder="输入车牌号筛选"
+          @select="setCarId"
+        ></el-autocomplete>
+        <el-select v-model="carTeamId" placeholder="选择车队筛选" clearable>
+          <el-option v-for="item in carTeamList" :key="item.carTeamId" :label="item.teamName" :value="item.carTeamId"></el-option>
+        </el-select>
       </el-form-item>
       <el-form-item label="检查日期" prop="checkDate">
         <el-date-picker v-model="formData.checkDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
@@ -11,7 +19,18 @@
         <el-input v-model="formData.checkPerson"></el-input>
       </el-form-item>
       <el-form-item label="附件照片名称" prop="accessoryNames">
-        <el-input v-model="formData.accessoryNames"></el-input>
+        <el-upload
+          :action="$baseURL + 'accessory/addAccessory'"
+          :file-list="picsList"
+          :on-success="handleUpload"
+          :on-remove="handleRemove"
+          :on-preview="handlePictureCardPreview"
+          list-type="picture-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="">
+        </el-dialog>
       </el-form-item>
       <el-form-item label="整改请求" prop="reformRequest">
         <el-input v-model="formData.reformRequest"></el-input>
@@ -28,8 +47,9 @@
 <script>
 import CheckList from './car-check'
 import saveMixin from '@/mixins/saveform'
+import uploadMixin from '@/mixins/upload'
 export default {
-  mixins: [saveMixin],
+  mixins: [uploadMixin, saveMixin],
   components: {
     CheckList
   },
@@ -45,16 +65,71 @@ export default {
         accessoryNames: '',
         reformRequest: ''
       },
+      picsList: [],
       rules: {},
       apiName: 'carCheckRecord/',
       addApi: 'addCarCheckRecord',
-      updateApi: 'updateCarCheckRecord'
+      updateApi: 'updateCarCheckRecord',
+      carTeamList: [],
+      carTeamId: '',
+      strPlateNum: ''
     }
   },
   mounted() {
     this.id && this.getDetail()
+    this.getCarTeamList()
   },
   methods: {
+    handleRemove(file, list) {
+      this.picsList = list
+    },
+    handleUpload(res) {
+      if (res.code == 0) {
+        this.picsList.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
+      }
+    },
+    async getCarName() {
+      let {data} = await this.$http({
+        url: '/car/getCar',
+        params: {
+          carId: this.formData.carId
+        }
+      })
+      if (data.code == 0) {
+        this.strPlateNum = data.data.plateNum
+      }
+    },
+    setCarId(val) {
+      this.formData.carId = val.id
+    },
+    async getCarList(keyword, cb) {
+      let {data} = await this.$http({
+        url: '/car/getCarListAll',
+        params: {
+          plateNum: keyword,
+          carTeamId: this.carTeamId
+        }
+      })
+      if (data.code == 0) {
+        cb(data.data.map(item => {
+          return {
+            id: item.carId,
+            value: item.plateNum
+          }
+        }))
+      }
+    },
+    async getCarTeamList() {
+      let {data} = await this.$http({
+        url: '/carTeam/getCarTeamListAll',
+        params: {
+          companyId: 1
+        }
+      })
+      if (data.code == 0) {
+        this.carTeamList = data.data
+      }
+    },
     async getDetail() {
       let {data} = await this.$http({
         url: 'carCheckRecord/getCarCheckRecord',
