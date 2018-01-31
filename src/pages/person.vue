84cf2777eb005e74285a47b0884a3ed873bb79b7
Noclip <noclip@foxmail.com>
2018-01-31 23:34:56 +0800

    授权开通登录

--- src/pages/person.vue
+++ src/pages/person.vue
@@ -39,8 +39,9 @@
       <el-table-column
         fixed="right"
         label="操作"
-        width="100">
+        width="180">
         <template slot-scope="scope">
+          <el-button @click="authClick(scope.row.mobile)" type="text" size="small">授权</el-button>
           <el-button @click="editClick(scope.row.personId)" type="text" size="small">编辑</el-button>
           <el-button @click="delectClick([scope.row.personId])" type="text" size="small">删除</el-button>
         </template>
@@ -55,6 +56,16 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="tableData.total">
     </el-pagination>
+    <el-dialog title="授权登录" :visible.sync="authShow">
+      <el-select v-model="authRole" placeholder="请选择角色">
+        <el-option
+        v-for="item in roleList"
+        :key="item.id"
+        :label="item.roleName"
+        :value="item.id"></el-option>
+      </el-select>
+      <el-button @click="authSubmit" type="primary">开通</el-button>
+    </el-dialog>
   </div>
 </template>
 
@@ -73,14 +84,53 @@ export default {
       params: {
         typeId: ''
       },
-      typeList: []
+      typeList: [],
+      authShow: false,
+      authRole: '',
+      authTel: '',
+      roleList: []
     }
   },
   mounted() {
     this.getList()
     this.getTypeList()
+    this.getRoleList()
   },
   methods: {
+    async authSubmit() {
+      if (this.authRole === '') {
+        this.$message.error('请选择角色')
+        return false
+      }
+      let {data} = await this.$http({
+        method: 'post',
+        url: 'auth/register',
+        data: {
+          password: '123456',
+          roleId: this.authRole,
+          userName: this.authTel
+        }
+      })
+      if (data.code == 0) {
+        this.$message({
+          message: '开通成功！',
+          type: 'success'
+        })
+        this.authShow = false
+      } else {
+        this.$message.error(data.msg)
+      }
+    },
+    authClick(tel) {
+      this.authTel = tel
+      this.authShow = true
+    },
+    async getRoleList() {
+      let {data} = await this.$http('role/getRoleList')
+      if (data.code == 0) {
+        this.roleList = data.data
+      }
+    },
     async getTypeList() {
       let {data} = await this.$http('personType/getPersonTypeList')
       if (data.code == 0) {
