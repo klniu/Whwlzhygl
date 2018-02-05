090293a4f3bb40c5a09f548ba573fa51fd3b6d0c
Noclip <noclip@foxmail.com>
2018-02-05 14:09:06 +0800

    客户管理调整

--- src/pages/customer-edit.vue
+++ src/pages/customer-edit.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="100px" size="medium">
-      <el-form-item label="客户地址" prop="customerAddress">
-        <el-input v-model="formData.customerAddress"></el-input>
-      </el-form-item>
       <el-form-item label="客户名称" prop="customerName">
         <el-input v-model="formData.customerName"></el-input>
+      </el-form-item>
+      <el-form-item label="客户地址" prop="customerAddress">
+        <el-input v-model="formData.customerAddress"></el-input>
       </el-form-item>
       <el-form-item label="联系人姓名" prop="linkmanName">
         <el-input v-model="formData.linkmanName"></el-input>
@@ -19,7 +19,29 @@
       <el-form-item label="发货地址" prop="sendAddress">
         <el-input v-model="formData.sendAddress"></el-input>
       </el-form-item>
-      <el-form-item label="证件" prop="pic">
+      <el-form-item label="合同图片" prop="contractPath">
+        <el-upload
+          :action="$baseURL + 'accessory/addAccessory'"
+          :file-list="picsList1"
+          :on-success="handleUpload1"
+          :on-remove="handleRemove1"
+          :on-preview="handlePictureCardPreview"
+          list-type="picture-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="营业执照图片" prop="bizLicensePath">
+        <el-upload
+          :action="$baseURL + 'accessory/addAccessory'"
+          :file-list="picsList2"
+          :on-success="handleUpload2"
+          :on-remove="handleRemove2"
+          :on-preview="handlePictureCardPreview"
+          list-type="picture-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="其他附件" prop="accessoryNames">
         <el-upload
           :action="$baseURL + 'accessory/addAccessory'"
           :file-list="picsList"
@@ -29,10 +51,10 @@
           list-type="picture-card">
           <i class="el-icon-plus"></i>
         </el-upload>
-        <el-dialog :visible.sync="dialogVisible">
-          <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog>
       </el-form-item>
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
       <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')" :loading="posting">保存</el-button>
         <el-button @click="resetForm('ruleForm')">重置</el-button>
@@ -56,9 +78,13 @@ export default {
         linkmanName: '',
         receiveAddress: '',
         sendAddress: '',
-        accessoryNames: ''
+        accessoryNames: '',
+        contractPath: '',
+        bizLicensePath: ''
       },
       picsList: [],
+      picsList1: [],
+      picsList2: [],
       rules: {},
       apiName: 'customer/',
       addApi: 'addCustomer',
@@ -77,6 +103,22 @@ export default {
         this.picsList.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
       }
     },
+    handleRemove1(file, list) {
+      this.picsList1 = list
+    },
+    handleUpload1(res) {
+      if (res.code == 0) {
+        this.picsList1.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
+      }
+    },
+    handleRemove2(file, list) {
+      this.picsList2 = list
+    },
+    handleUpload2(res) {
+      if (res.code == 0) {
+        this.picsList2.push({name: res.data.accessoryName, url: this.$baseURL + res.data.accessoryName})
+      }
+    },
     async getDetail() {
       let {data} = await this.$http({
         url: 'customer/getCustomer',
@@ -88,10 +130,14 @@ export default {
         this.formData = data.data
         this.formData.id = this.id
         this.picsList = this.pushPicInitList(this.formData.accessoryNames)
+        this.picsList1 = this.pushPicInitList(this.formData.contractPath)
+        this.picsList2 = this.pushPicInitList(this.formData.bizLicensePath)
       }
     },
     beforePost() {
       this.formData.accessoryNames = this.joinPicIntoString(this.picsList)
+      this.formData.contractPath = this.joinPicIntoString(this.picsList1)
+      this.formData.bizLicensePath = this.joinPicIntoString(this.picsList2)
     }
   }
 }
