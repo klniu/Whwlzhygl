410b4c1e0f0f2186fa4439e4fc79c9095c4798d6
Noclip <noclip@foxmail.com>
2018-02-07 23:22:51 +0800

    订单管理 分块 读取客户信息

--- src/pages/order-edit.vue
+++ src/pages/order-edit.vue
@@ -1,34 +1,50 @@
 <template>
   <div>
     <el-form :model="formData" :rules="rules" ref="ruleForm" label-width="150px" size="medium">
-      <el-form-item label="路单单号" prop="orderNum">
-        <el-input v-model="formData.orderNum"></el-input>
-      </el-form-item>
-      <el-form-item label="客户" prop="customerId">
-        <el-select v-model="formData.customerId" :loading="loadingCustomer" filterable>
-          <el-option
-            v-for="item in customerList"
-            :key="item.customerId"
-            :label="item.customerName"
-            :value="item.customerId"
-          ></el-option>
-          <div class="select-more" v-if="moreCustomerBt" @click="getCustomerList">点击加载更多</div>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="货物" prop="goodsId">
-        <el-select v-model="formData.goodsId" :loading="loadingGoods" filterable>
-          <el-option
-            v-for="item in goodsList"
-            :key="item.goodsId"
-            :label="item.goodsName"
-            :value="item.goodsId"
-          ></el-option>
-          <div class="select-more" v-if="moreGoodsBt" @click="getGoodsList">点击加载更多</div>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="货物重量" prop="goodsWeight">
-        <el-input v-model="formData.goodsWeight"></el-input>
-      </el-form-item>
+      <div class="form-title">客户信息</div>
+      <div class="form-block">
+        <el-form-item label="路单单号" prop="orderNum">
+          <el-input v-model="formData.orderNum"></el-input>
+        </el-form-item>
+        <el-form-item label="客户" prop="customerId">
+          <el-select v-model="formData.customerId" filterable @change="cusChange">
+            <el-option
+              v-for="item in customerList"
+              :key="item.customerId"
+              :label="item.customerName"
+              :value="item.customerId"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form :model="cusData" ref="cusForm" v-if="formData.customerId" label-width="150px">
+          <el-form-item label="联系号码" prop="linkmanMobile">
+            <el-input v-model="cusData.linkmanMobile"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="form-title">货物信息</div>
+      <div class="form-block">
+        <el-form-item label="货物" prop="goodsId">
+          <el-select v-model="formData.goodsId" :loading="loadingGoods" filterable>
+            <el-option
+              v-for="item in goodsList"
+              :key="item.goodsId"
+              :label="item.goodsName"
+              :value="item.goodsId"
+            ></el-option>
+            <div class="select-more" v-if="moreGoodsBt" @click="getGoodsList">点击加载更多</div>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="货物重量" prop="goodsWeight">
+          <el-input v-model="formData.goodsWeight"></el-input>
+        </el-form-item>
+      </div>
+      <div class="form-title">车辆信息</div>
+      <div class="form-block">
+      </div>
+      <div class="form-title">人员信息</div>
+      <div class="form-block">
+      </div>
       <el-form-item label="托运日期" prop="transportDate">
         <el-date-picker v-model="formData.transportDate" type="datetime" value-format="yyyy-MM-dd HH:mm"></el-date-picker>
       </el-form-item>
@@ -40,20 +56,12 @@
         <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
-    <div v-if="id">
-      <h3>电子路单详情列表：</h3>
-      <order-detail-list :id="id"></order-detail-list>
-    </div>
   </div>
 </template>
 <script>
-import OrderDetailList from './order-detail'
 import saveMixin from '@/mixins/saveform'
 export default {
   mixins: [saveMixin],
-  components: {
-    OrderDetailList
-  },
   data() {
     return {
       id: parseInt(this.$route.query.id),
@@ -66,14 +74,16 @@ export default {
         orderNum: '',
         transportDate: '',
       },
+      cusData: {
+        linkmanMobile: '',
+        sendAddress: '',
+        receiveAddress: ''
+      },
       rules: {},
       apiName: 'order/',
       addApi: 'addOrder',
       updateApi: 'updateOrder',
       customerList: [],
-      customerPage: 0,
-      loadingCustomer: false,
-      moreCustomerBt: false,
       goodsList: [],
       goodsPage: 0,
       loadingGoods: false,
@@ -86,6 +96,17 @@ export default {
     this.getGoodsList()
   },
   methods: {
+    async cusChange() {
+      let {data} = await this.$http({
+        url: 'customer/getCustomer',
+        params: {
+          customerId: this.formData.customerId
+        }
+      })
+      if (data.code == 0) {
+        this.cusData = data.data
+      }
+    },
     async getDetail() {
       let {data} = await this.$http({
         url: 'order/getOrder',
@@ -96,28 +117,19 @@ export default {
       if (data.code == 0) {
         this.formData = data.data
         this.formData.id = this.id
+        this.cusChange()
       }
     },
     async getCustomerList() {
-      this.customerPage ++
-      this.loadingCustomer = true
       let {data} = await this.$http({
-        url: 'customer/getCustomerList',
+        url: 'customer/getCustomerListAllContent',
         params: {
-          companyId: localStorage.getItem('companyId'),
-          currentPage: this.customerPage,
-          size: 50
+          companyId: localStorage.getItem('companyId')
         }
       })
       if (data.code == 0) {
-        this.moreCustomerBt = data.data.hasNextPage
-        if (data.data.list.length > 0) {
-          this.customerList = this.customerList.concat(data.data.list)
-        } else {
-          this.$message('没有更多记录')
-        }
+        this.customerList = data.data
       }
-      this.loadingCustomer = false
     },
     async getGoodsList() {
       this.goodsPage ++
@@ -149,5 +161,17 @@ export default {
   text-align: center;
   color: #aaa;
   cursor: pointer;
+}
+.form-title{
+  margin: 10px 0;
+}
+.form-block{
+  padding: 10px;
+  border: 1px solid #ebebeb;
+  border-radius: 3px;
+  transition: .2s;
+}
+.form-block:hover{
+  box-shadow: 0 0 8px 0 rgba(232,237,250,.6), 0 2px 4px 0 rgba(232,237,250,.5);
 }
 </style>
